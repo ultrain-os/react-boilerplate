9f9f74dec083c357a75641590e1c8c8775c21eda
benyasin <rgwapkk520>
2019-02-13 13:44:46 +0800

    change build to dist

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require("path");
+process.env.NODE_ENV = 'production'
+var reactScriptsConfig = require('react-scripts/config/webpack.config.prod')
+module.exports = Object.assign({}, reactScriptsConfig, {
+    output: Object.assign({}, reactScriptsConfig.output, {
+        path: path.resolve(__dirname, "./dist"),
+    })
+})
